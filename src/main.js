8164fe0cad109f017b34f6b9ad447e55aba8530a
范浩德 <1779017652@qq.com>
2022-04-26 02:28:00 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import './plugins/element.js'
 // 导入全局样式表
 import './assets/css/global.css'
 
+import TreeTable from 'vue-table-with-tree-grid'
+
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://175.27.228.178:3434/api/private/v1/'
@@ -16,6 +18,8 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+
+Vue.component('tree-table', TreeTable)
 
 new Vue({
   router,
